67db8fb1463a55e82510ce9c68f87eb757cdd01d
Jarles <62815538+Jarlez@users.noreply.github.com>
2023-09-19 17:51:21 -0300

    Update address.model.js

--- src/models/address.model.js
+++ src/models/address.model.js
@@ -5,7 +5,7 @@ const addressSchema = new mongoose.Schema({
     user_id: { type: String, required: true },
     street: { type: String, required: true },
     neighborhood: { type: String, required: true },
-    postal_code: { type: String, required: true },
+    postalCode: { type: String, required: true },
     city: { type: String, required: true },
     state: { type: String, required: true },
     country: { type: String, required: true },
